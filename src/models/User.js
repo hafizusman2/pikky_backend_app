5217b21fcec9c0fd3ee449be4229e591d76c825c
Hafiz Usman <hafizusmanismail2@gmail.com>
2024-11-13 23:59:42 +0500

    Add User model with schema definition, password hashing, and role management

---
+++ src/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user"
+    }
+  },
+  {
+    timestamps: true,
+    indexes: [{ fields: { email: 1 } }, { fields: { role: 1 } }]
+  }
+);
+
+userSchema.index({ email: 1 });
+userSchema.index({ role: 1 });
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
